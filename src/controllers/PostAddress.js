d8d31e65f7ba2ce4e74af431c7eecf405e8a9b95
teerenzo <renzahoemmanuel8@gmail.com>
2023-01-26 11:16:17 +0200

    address avalidate

--- src/controllers/PostAddress.js
+++ src/controllers/PostAddress.js
@@ -4,6 +4,13 @@ const postAdrress = async (req,res)=>{
  try {
    Jwt.verify(req.headers.authorization.split(' ')[1], process.env.TOKEN_SECRET)
    console.log(req.user)
+   const addressFound= await Address.findOne({
+      where:{
+          userId: Jwt.verify(req.headers.authorization.split(' ')[1], process.env.TOKEN_SECRET).user.id
+      }
+  })
+  if(addressFound) return res.status(202).json({"error":'address already unserted try update'});
+
     const address = await Address.create({
         Province: req.body.Province,
         District: req.body.District,
